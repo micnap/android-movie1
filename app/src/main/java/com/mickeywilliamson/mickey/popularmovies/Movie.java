e047d5d3b1d4f52823451bc42796c522feb3e3c3
Michelle Williamson <mwill31@uis.edu>
2018-05-30 19:47:08 -0400

    Movie POJO

--- app/src/main/java/com/mickeywilliamson/mickey/popularmovies/Movie.java
+++ app/src/main/java/com/mickeywilliamson/mickey/popularmovies/Movie.java
@@ -1,4 +1,52 @@
 package com.mickeywilliamson.mickey.popularmovies;
 
+import java.util.Date;
+
 public class Movie {
+    private String title;
+    private String image;
+    private String plot;
+    private int rating;
+    private Date releaseDate;
+
+
+    public void Movie() {}
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setPlot(String plot) {
+        this.plot = plot;
+    }
+
+    public String getPlot() {
+        return plot;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setReleaseDate(Date date) {
+        this.releaseDate = date;
+    }
 }
+
+
